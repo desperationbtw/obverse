3951808f8e9dc8158a447bf4c3ca7b2f403c2dec
Dmitry <desperation.btw@gmail.com>
2020-07-01 22:03:08 +1000

    stealth bloked 'languages' setter, need rework

--- src/renderer/components/modules/fingerprint/fpByPass.js
+++ src/renderer/components/modules/fingerprint/fpByPass.js
@@ -1,13 +1,15 @@
 const puppeteer = require("puppeteer-extra");
 const stealth = require("puppeteer-extra-plugin-stealth")();
-const proxyServer = require('../proxy/proxyServer');
-const { proxyChecker } = require('../proxy/proxyChecker');
+const path = require("path");
+const rimraf = require("rimraf");
+const proxyServer = require("../proxy/proxyServer");
+const { proxyChecker } = require("../proxy/proxyChecker");
 const { UAGenerator } = require("./userAgent");
 const canvasGenerator = require("./canvas");
 const utils = require("../utils");
 
-stealth.onBrowser = () => {};
-puppeteer.use(stealth);
+// stealth.onBrowser = () => {};
+// puppeteer.use(stealth);
 
 async function runBrowserSession(options) {
   var defaults = {
@@ -16,13 +18,31 @@ async function runBrowserSession(options) {
     proxy: null,
     userAgent: new UAGenerator().Alone(),
     canvas: canvasGenerator.generateCanvas(),
+    sessionPath: "guest",
+    doNotTrack: true,
   };
 
   utils.setDefaults(options, defaults);
+  function browserDataPath(fpath) {
+    return `${
+      process.env.NODE_ENV === "development"
+        ? path.join(__static, "data", "session", String(fpath))
+        : path.join(
+            app.getAppPath("userData"),
+            "data",
+            "session",
+            String(fpath)
+          )
+    }`;
+  }
+
+  rimraf(browserDataPath("guest"), function() {
+    console.log("Rimraf: guest path cleared");
+  });
 
   const args = [
     "--no-sandbox",
-    "--lang=en-GB",
+    `--lang=${options.timeZone[1]},${options.timeZone[0]}`,
     "--disable-setuid-sandbox",
     "--disable-infobars",
     "--window-position=0,0",
@@ -41,11 +61,15 @@ async function runBrowserSession(options) {
   }
 
   const _options = {
+    env: {
+      TZ: options.timeZone[2],
+      ...process.env,
+    },
     args,
     headless: options.headless,
     ignoreHTTPSErrors: true,
     defaultViewport: null,
-    //userDataDir: "./tmp", //?Для сейва сессий в будущем
+    userDataDir: browserDataPath(options.sessionPath),
   };
 
   var browser = await puppeteer.launch(_options);
@@ -61,9 +85,10 @@ async function runBrowserSession(options) {
     await page.setUserAgent(options.userAgent.ua);
     await page.setExtraHTTPHeaders({
       "Accept-Language": options.timeZone[0],
+      DNT: options.doNotTrack ? "1" : "0",
     });
 
-    await page.evaluateOnNewDocument(() => {
+    await page.evaluateOnNewDocument((options) => {
       const originalFunction = HTMLCanvasElement.prototype.toDataURL;
       HTMLCanvasElement.prototype.toDataURL = function(type) {
         if (type === "image/png" && this.width === 220 && this.height === 30) {
@@ -71,7 +96,21 @@ async function runBrowserSession(options) {
         }
         return originalFunction.apply(this, arguments);
       };
-    });
+
+      Object.defineProperty(navigator, "language", {
+        configurable: false,
+        enumerable: true,
+        get: function() {return options.timeZone[1]},
+      });
+      navigator.languages = [options.timeZone[1], options.timeZone[0]];
+      console.log(navigator.languages);
+      Object.defineProperty(navigator, "languages", {
+        configurable: false,
+        enumerable: true,
+        get: function() {return [options.timeZone[1], options.timeZone[0]]},
+      });
+    }, options);
+    console.log(navigator.languages);
     return page;
   };
 
